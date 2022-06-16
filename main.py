e7fb2f9bcef79821af858ccb2f0d1af93e2ce513
Kassidy Holm <kassidyjo97@gmail.com>
2022-06-16 18:51:01 -0500

    add variables to make adjusting display size easier

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
 pygame.init()
 
-dis=pygame.display.set_mode((800,600))
+
+dis_width=800
+dis_height=600
+dis=pygame.display.set_mode((dis_width,dis_height))
 pygame.display.set_caption('Snake game by CallMeKass')
 
 blue=(0,0,255)
@@ -11,8 +14,8 @@ white = (255, 255, 255)
 run_game=True
 debugger=True
 
-x1 = 400
-y1 = 300
+x1 = int(dis_width/2)
+y1 = int(dis_height/2)
  
 x1_change = 0       
 y1_change = 0
